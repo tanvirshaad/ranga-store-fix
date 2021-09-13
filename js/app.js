d8ca1ce18dd67b72cc171e3b84ab9d096eb3f7e7
tanvirshaad <tanvirshaad@gmail.com>
2021-09-13 17:36:09 +0600

    grandTotal issue and bg-color updated

--- js/app.js
+++ js/app.js
@@ -212,15 +212,18 @@ const showProducts = (products) => {
         const div = document.createElement('div');
         div.classList.add('product');
         div.innerHTML = `
-        <div class="single-product">
-        <div>
-          <img class="product-image" src=${product.image}></img>
-        </div>
-        <h3>${product.title}</h3>
-        <p>Category: ${product.category}</p>
-        <h2>Price: $ ${product.price}</h2>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button>
+        <div class="single-product card">
+            <img src="${product.image}" class="product-image card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">
+                    <p>Category: ${product.category}</p>
+                    <h2>Price: ${product.price}</h2>
+                </p>
+                <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to
+                cart</button>
+                <button id="details-btn" class="btn btn-danger">Details</button>
+            </div>
         </div>
       `;
         document.getElementById('all-products').appendChild(div);
@@ -277,6 +280,6 @@ const updateTotal = () => {
         getInputValue('price') +
         getInputValue('delivery-charge') +
         getInputValue('total-tax');
-    document.getElementById('total').innerText = grandTotal;
+    document.getElementById('total').innerText = grandTotal.toFixed(2);
 };
 loadProducts();
