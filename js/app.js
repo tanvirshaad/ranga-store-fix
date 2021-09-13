4b88fd9eaf18f75973d629d4839207d4cbee671d
tanvirshaad <tanvirshaad@gmail.com>
2021-09-13 10:44:56 +0600

    decimal number issue solved

--- js/app.js
+++ js/app.js
@@ -237,7 +237,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
     const element = document.getElementById(id).innerText;
-    const converted = parseInt(element);
+    const converted = parseFloat(element);
     return converted;
 };
 
@@ -246,12 +246,12 @@ const updatePrice = (id, value) => {
     const convertedOldPrice = getInputValue(id);
     const convertPrice = parseFloat(value);
     const total = convertedOldPrice + convertPrice;
-    document.getElementById(id).innerText = Math.round(total);
+    document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-    document.getElementById(id).innerText = Math.round(value);
+    document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
